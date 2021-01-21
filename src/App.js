65dfa1223ebfb1dde151243c81185de34610f5bc
ri003074 <ri003074@gmail.com>
2021-01-21 13:44:22 +0900

    use axios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        axios.get('https://enigmatic-stream-15237.herokuapp.com/api/tasks/')
+            .then(res => {
+                setTasks(res.data);
+                console.log(res.data)
+            })
+    }, [])
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello World</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
-            </header>
+        <div>
+            {
+                tasks.map((task, index) =>
+                    <div key={task.id}>{task.title}</div>
+                )
+            }
         </div>
     );
 }
