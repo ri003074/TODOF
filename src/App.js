659771c8ae5f21fcf89372e164d009a4fb646107
ri003074 <ri003074@gmail.com>
2021-01-23 14:23:04 +0900

    use token

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ const App = () => {
     const [tasks, setTasks] = useState([])
 
     useEffect(() => {
-        axios.get('https://enigmatic-stream-15237.herokuapp.com/api/tasks/')
+        axios.get('https://enigmatic-stream-15237.herokuapp.com/api/tasks/',
+            {
+                headers: {
+                    // Authorization: localStorage.getItem('token'),
+                    Authorization: "Token b5fe4b8aeb4ecc6bd9587cf2bcb5428dd2e7d965"
+                }
+            })
             .then(res => {
                 setTasks(res.data);
                 console.log(res.data)
